9f294e7d5b759cdf9b031f50706fa366cf44adef
minhchinhctk41 <minhchinh04@gmail.com>
2021-09-05 18:07:44 +0700

    Deploy Trello api to Heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { corsOptions } from '*/config/cors'
 import { connectDB } from '*/config/mongodb'
-import { env } from '*/config/environtment'
+// import { env } from '*/config/environtment'
 import { apiV1 } from '*/routes/v1'
 
 connectDB()
@@ -24,7 +24,12 @@ const bootServer = () => {
   // Use APIs v1
   app.use('/v1', apiV1)
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`Server is running in ${env.APP_HOST}:${env.APP_PORT}`)
+  // app.listen(env.APP_PORT, env.APP_HOST, () => {
+  //   console.log(`Server is running in ${env.APP_HOST}:${env.APP_PORT}`)
+  // })
+
+  // Support heroku deploy
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running in :${process.env.PORT}`)
   })
 }
